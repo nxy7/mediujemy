80704a20300b8455a5a07e6ff22978bc52125895
nxy7 <lolnoxy@gmail.com>
2023-01-24 20:31:01 +0100

    path fix

--- components/navbar.vue
+++ components/navbar.vue
@@ -4,7 +4,7 @@
         <!-- desktop -->
         <ul class="hidden md:flex w-full justify-center items-center space-x-12">
             <a href="/">
-                <img src="logo1.png" class="self-start h-12" />
+                <img src="/logo1.png" class="self-start h-12" />
             </a>
             <a href="zespol">Nasz zespół</a>
             <a href="mediacje">O mediacji</a>
