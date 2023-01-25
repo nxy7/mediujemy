1394fc3c9eb8af86c83c50c238a2ddc7f96a7ae2
nxy7 <lolnoxy@gmail.com>
2023-01-25 02:16:47 +0100

    a swapped for nuxtlink

--- components/navbar.vue
+++ components/navbar.vue
@@ -3,14 +3,14 @@
     <div class="sticky w-full top-0 flex justify-center items-center bg-white z-40 shadow-lg h-16">
         <!-- desktop -->
         <ul class="hidden md:flex w-full justify-center items-center space-x-12">
-            <a href="/">
+            <NuxtLink href="/">
                 <img src="/logo1.png" class="self-start h-12" />
-            </a>
-            <a href="zespol">Nasz zespół</a>
-            <a href="mediacje">O mediacji</a>
-            <a href="cennik">Cennik</a>
-            <a href="aktualnosci">Aktualności</a>
-            <a href="kontakt">Kontakt</a>
+            </NuxtLink>
+            <NuxtLink href="zespol">Nasz zespół</NuxtLink>
+            <NuxtLink href="mediacje">O mediacji</NuxtLink>
+            <NuxtLink href="cennik">Cennik</NuxtLink>
+            <NuxtLink href="aktualnosci">Aktualności</NuxtLink>
+            <NuxtLink href="kontakt">Kontakt</NuxtLink>
         </ul>
 
         <!-- mobile -->
