e04dc1234a114454a14338a2c0b995341d11710f
nxy7 <lolnoxy@gmail.com>
2023-01-25 01:25:43 +0100

    navbar link fix

--- components/navbar.vue
+++ components/navbar.vue
@@ -6,11 +6,11 @@
             <a href="/">
                 <img src="/logo1.png" class="self-start h-12" />
             </a>
-            <a href="zespol">Nasz zespół</a>
-            <a href="mediacje">O mediacji</a>
-            <a href="cennik">Cennik</a>
-            <a href="aktualnosci">Aktualności</a>
-            <a href="kontakt">Kontakt</a>
+            <a href="/zespol">Nasz zespół</a>
+            <a href="/mediacje">O mediacji</a>
+            <a href="/cennik">Cennik</a>
+            <a href="/aktualnosci">Aktualności</a>
+            <a href="/kontakt">Kontakt</a>
         </ul>
 
         <!-- mobile -->
