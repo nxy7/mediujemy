7f096c55b0d55d8e4b18d53b892b818a3fde1f9b
Dawid Danieluk <danielukd@gmail.com>
2024-08-06 13:01:07 +0200

    WIP

--- pages/zespol.vue
+++ pages/zespol.vue
@@ -14,7 +14,8 @@
 </template>
 
 <script setup lang="ts">
-// import { member } from "~~/components/member/member";
+import type { member } from '~/components/member/member';
+
 // let base = process.env.NUXT_APP_BASE_URL || "/"
 let base = useRuntimeConfig().public.baseUrl;
 console.log("base", base);
@@ -51,10 +52,15 @@ W swojej pracy wykorzystuję elementy TSR (Terapia Skoncentrowana na Rozwiązani
 Moją ogromną pasją jest rozwój osobisty, wierzę, że każdy z nas ma potencjał, który czasami drzemie w nas ukryty i potrzebuje małego wsparcia aby ujrzeć światło dzienne.
 Stąd prowadzone przeze mnie coachingi i warsztaty uwzględniają  tematykę związaną z wypaleniem zawodowym, radzeniem sobie ze stresem, wzmocnieniem poczucia własnej wartości,
  poprawa komunikacji i rozwiazywaniem konfliktów.`
+``,``,``,
 
-,`Od ponad 7 lat jestem również praktykiem mindfulness. 
-Obecnie pogłębiam swój warsztat w Polskim Instutucie Minfulness przygotowując się do kursu nauczycielskiego.
-    `],
+`Z wykształcenia jestem prawnikiem (UMCS) oraz mediatorem wpisanym na listę stałych mediatorów przy Sądzie Okręgowym w Lublinie.
+Ukończyłam studia podyplomowe z zakresu mediacji szkolnych i sądowych (UMCS) i zaawansowane kursy z mediacji rodzinnych. `
+,
+`Jako absolwentka Szkoły Trenerów Grupy SET posiadam certyfikat Trenera Biznesu. Warsztat  coachingowy pogłębiałam w Akademii Instytutu Pozytywnej Terapii Kryzysu (ukończone dwa poziomy: Konsultant Kryzysowy oraz Coach i Mentor Kryzysowy).`,
+`Od ponad 7 lat jestem również praktykiem mindfulness. Obecnie pogłębiam swój warsztat w Polskim Instytucie Minfulness przygotowując się do kursu nauczycielskiego.
+`
+,],
     desc: "Mediator, coach kryzysowy oraz trener biznesu.",
   },
 ];
